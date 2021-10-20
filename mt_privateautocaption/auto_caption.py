84fa62e2c6fd07821e0db347bfca55dd57ebf4a9
Ǥнσsτ♛ <86915159+arjunsangu@users.noreply.github.com>
2021-10-20 11:12:16 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -16,10 +16,10 @@ URL_LINK=Config.URL_LINK
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
-    await message.edit(f"<b>{kopp.file_name}</b>\n\n{CAPTION_TEXT}",
+    await message.edit(f"<b>{kopp.file_name}</b>",
           reply_markup=InlineKeyboardMarkup(
               [[
-              InlineKeyboardButton(f"{BUTTON_TEXT}", url=f"{URL_LINK}")
+              InlineKeyboardButton("💢 NEW MOVIES 💢", url="https://t.me/MLM_ROKES")
               ]]
         ))
 

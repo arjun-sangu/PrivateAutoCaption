4a3edf405dc4794eae98540c5118165d4e404408
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-03-28 07:33:30 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -3,20 +3,19 @@
 # (c) Shrimadhav U K & PR0FESS0R-99
 
 import os
-from config import Config
 from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 
-CAPTION_TEXT=Config.CAPTION
-BUTTON_TEXT=Config.BUTTON_TEXT
-URL_LINK=Config.URL_LINK
-
+CAPTION_TEXT = os.environ.get("CAPTION", "@MALLU_ROKERS")
+BUTTON_TEXT = os.environ.get("BUTTON", "🔻Join Channel🔻")
+URL_LINK = os.environ.get("LINK", "T.ME/MO_TECH_YT")
+  
 
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
-    await message.edit(f"<b>{kopp.file_name}</b>",
+    await message.edit(f"`{kopp.file_name}`",
           reply_markup=InlineKeyboardMarkup(
               [[
               InlineKeyboardButton(f"{BUTTON_TEXT}", url=f"{URL_LINK}")
